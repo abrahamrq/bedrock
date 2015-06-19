7d3955b90bfafbea52d2d9363e3c2d543d0bfb38
abrahamrq <abraham.rq03@gmail.com>
2015-06-18 22:09:14 -0500

    add links to sessions

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.0'
+gem 'i18n', github: 'svenfuchs/i18n'
 gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_locale
+    I18n.locale = user_signed_in? ? current_user.local.to_sym : 
+    	I18n.default_locale
+  end
 end

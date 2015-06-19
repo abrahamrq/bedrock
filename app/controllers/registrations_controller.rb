f5e7df2cb8fdf29178963be1f7cc120e4e52b2a9
abrahamrq <abraham.rq03@gmail.com>
2015-06-19 02:54:37 -0500

    Translate all devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,2 @@
 class RegistrationsController < Devise::RegistrationsController
-	def set_locale
-		new_locale = params[:locale]
-		if ['es', 'en'].include?(new_locale)
-			current_user.update_attributes(locale: new_locale)
-			I18n.locale = new_locale.to_sym
-			flash[:success] = t('locale_changed')
-			redirect_to :back
-		else
-			flash[:error] = t('locale_changed_error')
-			redirect_to :back
-		end
-	end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   root 'welcome#home'
 
-  devise_scope :user do
-  	post 'set_locale', to: 'registrations#set_locale', as: :set_user_locale
-	end
+  post '/set_locale/wow', to: 'users#set_locale', as: :set_user_locale
 end

226c30e384bf93431bc149168b7272ae2279a20f
abrahamrq <abraham.rq03@gmail.com>
2015-06-18 21:10:55 -0500

    Add nav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,5 +22,25 @@
 //= require twitter/bootstrap
 //= require moment
 
+$(document).ready(function () {	
+
+  var toggleMenu = function() {
+	  $('header').toggleClass('toggle');
+	  $('.main').toggleClass('push');
+	  $('.overlay').toggleClass('block');
+	  $('#social, .logo').toggleClass('reveal');
+  };
+
+	//Nav
+	$('.navBtn').click(function() {
+    toggleMenu();
+	});
+
+  Mousetrap.bind('esc', function() {
+    toggleMenu();
+  });
+
+});
+
 
 //= require turbolinks
